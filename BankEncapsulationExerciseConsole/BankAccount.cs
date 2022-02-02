8d1ff8cd9d0738574b0c1a060745d082034357c2
Supersagee <williamdsage@gmail.com>
2022-02-01 20:27:53 -0600

    initial commit

---
+++ BankEncapsulationExerciseConsole/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExerciseConsole
+{
+    public class BankAccount
+    {
+        private double balance;
+        
+        public BankAccount()
+        {
+
+        }
+
+        public void Deposit(double depositAmount)
+        {
+            balance += depositAmount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+
+        public double GetBalance()
+        {
+            
+            return balance;
+        }
+    }
+
+}
---
+++ BankEncapsulationExerciseConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BankEncapsulationExerciseConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var account = new BankAccount();
+
+            Console.WriteLine("How much money would you like to deposit?");
+            double userDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(userDeposit);
+
+            Console.WriteLine("Would you like to see the balance of your account? If you would, type YES");
+            var userResponse = Console.ReadLine();
+
+            if (userResponse == "YES")
+            {
+                Console.WriteLine($"Your account balance is {account.GetBalance()}. Have a wonderful day!");
+            }
+            else
+            {
+                Console.WriteLine($"Have a wonderful day!");
+            }
+
+
+        }
+    }
+}
